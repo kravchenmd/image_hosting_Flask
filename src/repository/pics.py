566483eb24f16d237e15ec3df1afeb4550a98e6c
Kravchenko M <kravchenmd@gmail.com>
2022-09-27 18:50:30 +0200

    Revert "Remove unused `user` from `upload_file_for_user`"
    This reverts commit 4a32feae4f88caaaf665a31d4b67fb5ce9f5ea2c.

--- src/repository/pics.py
+++ src/repository/pics.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from src import db
 from src import models
 from src.libs.file_service import move_picture
+from src.repository.users import find_by_id
 
 
 def get_pictures_user(user_id: int):
@@ -10,6 +11,7 @@ def get_pictures_user(user_id: int):
 
 
 def upload_file_for_user(user_id: int, file_path: Path, description: str) -> None:
+    user = find_by_id(user_id)
     filename, size = move_picture(user_id, file_path)
     picture = models.Picture(path=filename, description=description, user_id=user_id, size=size)
     db.session.add(picture)
