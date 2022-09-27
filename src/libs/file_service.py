13cee9c0e9d79a797de723413c09d80dc4a63de8
Kravchenko M <kravchenmd@gmail.com>
2022-09-27 19:02:46 +0200

    Edit naming the folder in which file will be moved after uploading, to include username

--- src/libs/file_service.py
+++ src/libs/file_service.py
@@ -4,15 +4,15 @@ from config.config import BASE_DIR
 from pathlib import Path
 
 
-def move_picture(user_id: int, file_path: Path) -> tuple[str, int]:
+def move_picture(user_subfolder: str, file_path: Path) -> tuple[str, int]:
     """
     Move, rename picture after uploading.
     Return relative path to the file and size for storing in the DB
     """
-    target_folder = Path(BASE_DIR / "src" / "static" / str(user_id))
+    target_folder = Path(BASE_DIR / "src" / "static" / user_subfolder)
     target_folder.mkdir(exist_ok=True)
     # add time stamp in filename (to exclude duplicated names)
     file = file_path.rename(target_folder / Path(str(datetime.now().strftime("%I_%M_%S")) + file_path.name))
     size = file.stat().st_size
-    file_name_for_db = f"/static/{user_id}/{file.name}"  # relative to `static` path for the DB
+    file_name_for_db = f"/static/{user_subfolder}/{file.name}"  # relative to `static` path for the DB
     return file_name_for_db, size
--- src/repository/pics.py
+++ src/repository/pics.py
@@ -12,7 +12,8 @@ def get_pictures_user(user_id: int):
 
 def upload_file_for_user(user_id: int, file_path: Path, description: str) -> None:
     user = find_by_id(user_id)
-    filename, size = move_picture(user_id, file_path)
+    user_subfolder = f"{user_id}_{user.username}"
+    filename, size = move_picture(user_subfolder, file_path)
     picture = models.Picture(path=filename, description=description, user_id=user_id, size=size)
     db.session.add(picture)
     db.session.commit()
