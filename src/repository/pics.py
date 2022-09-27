196a389600049b54051cb424abf9ecea24b98a88
Kravchenko M <kravchenmd@gmail.com>
2022-09-27 17:50:35 +0200

    Add functions to move and add picture to the DB after uploading

--- src/repository/pics.py
+++ src/repository/pics.py
@@ -1,6 +1,18 @@
+from pathlib import Path
+
 from src import db
 from src import models
+from src.libs.file_service import move_picture
+from src.repository.users import find_by_id
 
 
-def get_pictures_user(user_id):
+def get_pictures_user(user_id: int):
     return db.session.query(models.Picture).where(models.Picture.user_id == user_id).all()
+
+
+def upload_file_for_user(user_id: int, file_path: Path, description: str) -> None:
+    user = find_by_id(user_id)
+    filename, size = move_picture(user_id, file_path)
+    picture = models.Picture(pat=filename, description=description, user_id=user_id, size=size)
+    db.session.add(picture)
+    db.session.commit()
